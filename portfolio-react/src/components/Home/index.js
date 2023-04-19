2e2fda759d874a0b0c5f5e5ef8e5f4318fca92d2
cee breezy <djmisscbrown@hotmail.com>
2023-04-19 10:31:39 +0100

    index and scss files for home page

---
+++ portfolio-react/src/components/Home/index.js
@@ -0,0 +1,68 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+import Loader from 'react-loaders';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a', 'r', 'l', 'a']
+    const jobArray = [
+        'w',
+        'e',
+        'b',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.'
+    ]
+
+    useEffect(() => {
+        setTimeout(() => {
+        setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <>
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>                   
+                    <br /> 
+                    <span className={`${letterClass} _13`}>I'</span> 
+                    <span className={`${letterClass} _14`}>m</span> 
+                <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                   <AnimatedLetters letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    /> 
+                <br />
+                
+                    </h1>
+                <h2>Frontend Developer / Javascript / No Code</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo></Logo>
+            </div>
+            <Loader type='pacman' />
+        </>
+    );
+}
+
+export default Home
